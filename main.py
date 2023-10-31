08610290750da4d6e48e5b2e4c5bb6d2911ed4e0
izzystorm10 <marin.isabella.10@gmail.com>
2023-10-31 17:10:03 -0400

    Isabella's changes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Aadithi Arjun
+# Lab partner: Isabella Marin
+
 def encode(password):
     encoded_password = ''
     for digit in str(password):
@@ -6,6 +8,16 @@ def encode(password):
         encoded_password += str(new_digit)
     return encoded_password
 
+# decode function done by lab partner, Isabella Marin
+def decode(encoded_password):
+    # initialize empty string for decoded password
+    decoded_password = ""
+    # loop through the digits in the string encoded password
+    for digit in str(encoded_password):
+        new_password = int(digit) - 3               # convert characters in string to integers & subtract 3 from each
+        decoded_password += str(new_password)       # convert the new password back to a string and add to empty string
+
+    return decoded_password
 
 # main function
 def main():
@@ -23,14 +35,13 @@ def main():
             UserChoice1 = int(input("Please enter your password to encode:"))
             print(UserChoice1)
             print("Your password has been encoded and stored!")
-
+            global x
             x = encode(UserChoice1)
 
         # option2 -> encode and decode password
         elif option == '2':
-            print("a")
-
-            # print("The encoded password is", encoded_password, "and the original password is", UserChoice1)
+            decoded_password = decode(x)
+            print(f"The encoded password is {x}, and the original password is {decoded_password}.")
 
         # option3 -> quit
         elif option == '3':
